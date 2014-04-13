5e4fa757a9fdee4300d153b8f3733c0cfeb16e6c
Paul Maley <maleyenator@gmail.com>
2014-04-13 18:31:34 +0200

    Added a test markdown file

---
+++ TestScript.R
@@ -0,0 +1,7 @@
+#
+# Totally useless script to test Git
+# 
+
+version <- "0.0.1"
+msg <- paste("Test Script version ", version, sep="")
+print(msg)
---
+++ mdTest.md
@@ -0,0 +1,9 @@
+﻿# Markdown Test
+
+## Section 1
+
+## Section 2
+
+Here is a list
+* Item 1
+* Item 2 * Is this a third item?
